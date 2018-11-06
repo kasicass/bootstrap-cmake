d893323cd31a2489f04d5e5a6a064264a40aa4f2
kasicass <kasicass@gmail.com>
2018-11-07 06:09:26 +0800

    prepare

---
+++ 3rdparty/clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+cd 3rdparty
+rm -rf boost-cmake
+rm -rf boost
+
---
+++ 3rdparty/prepare.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh env
+
+cd 3rdparty
+
+# boost-cmake
+if [ ! -d "boost-cmake" ]; then
+	echo "prepare boost-cmake"
+	wget https://github.com/Orphis/boost-cmake/archive/v1.67.0.tar.gz
+	tar zxf v1.67.0.tar.gz
+	mv boost-cmake-1.67.0 boost-cmake
+	rm v1.67.0.tar.gz
+fi  
+
+# boost
+if [ ! -d "boost" ]; then  
+	echo "prepare boost"
+	wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+	tar zxf boost_1_67_0.tar.gz
+	mv boost_1_67_0 boost
+	rm boost_1_67_0.tar.gz
+fi
+
